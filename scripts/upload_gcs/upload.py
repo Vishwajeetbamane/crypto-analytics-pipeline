928f4fc0b846b9835f5181e634bd70f60d9cd2aa
Vishwajeet <bamanevishwajeet@gmail.com>
2026-04-18 03:29:39 +0530

    commit project folder

---
+++ scripts/upload_gcs/upload.py
@@ -0,0 +1,46 @@
+
+from google.cloud import storage
+from pathlib import Path
+from datetime import datetime, timezone
+import os
+
+
+year = datetime.now(timezone.utc).strftime("%Y")
+month = datetime.now(timezone.utc).strftime("%m")
+day = datetime.now(timezone.utc).strftime("%d")
+hour = datetime.now(timezone.utc).strftime("%H")
+
+project_path = Path(__file__).resolve().parent.parent.parent
+source = Path(f"{project_path}/parquet/data.parquet")
+
+def upload_blob(source):
+    """Uploads a file to the bucket."""
+
+    # The ID of your GCS bucket
+    bucket_name = "crypto_project1"
+
+    # The path to your file to upload
+
+    source_file_name = source
+
+    # The ID of your GCS object
+    destination_blob_name = f"parquet/year={year}/month={month}/day={day}/hour={hour}/data.parquet"
+
+    storage_client = storage.Client()
+    bucket = storage_client.bucket(bucket_name)
+    blob = bucket.blob(destination_blob_name)
+
+
+    blob.upload_from_filename(source_file_name)
+
+    print(
+        f"File {source_file_name} uploaded to {destination_blob_name}."
+    )
+
+# Delete unwanted file
+def del_raw(source):
+    os.remove(source)
+    
+
+upload_blob(source)
+del_raw(source)
---
+++ scripts/warehouse/load_to_bq.py
@@ -0,0 +1,27 @@
+from google.cloud import bigquery
+
+def create_external_table():
+    client = bigquery.Client()
+
+    table_id = "dtc-course-486211.crypto_project1.crypto_data_ext"
+
+    external_config = bigquery.ExternalConfig("PARQUET")
+    external_config.source_uris = [
+        "gs://crypto_project1/parquet/*"
+    ]
+
+    # Enable Hive partitioning
+    hive_partitioning_opts = bigquery.external_config.HivePartitioningOptions()
+    hive_partitioning_opts.mode = "AUTO"
+    hive_partitioning_opts.source_uri_prefix = "gs://crypto_project1/parquet/"
+
+    external_config.hive_partitioning = hive_partitioning_opts
+
+    table = bigquery.Table(table_id)
+    table.external_data_configuration = external_config
+
+    table = client.create_table(table, exists_ok=True)
+
+    print(f"Created table {table.table_id}")
+
+create_external_table()
